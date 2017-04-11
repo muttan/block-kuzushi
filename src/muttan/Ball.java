538d1250d4deaae68ce7bad9e12820756c7fd56f
muttan <muttanmaiden@gmail.com>
2017-04-11 23:49:08 +0900

    angleとspeedの調整をした！

--- src/muttan/Ball.java
+++ src/muttan/Ball.java
@@ -9,27 +9,27 @@ import java.util.stream.Collectors;
  * Created by muttan on 2017/03/01.
  */
 public class Ball extends RectangleGameObject {
-    private static SpriteSheet sheet;
-    private Image sprite;
-    protected float dx = 0.5f;
-    protected float dy = 0.5f;
-    protected Bar stickingTarget;
-    protected float relative;
     public static final float WIDTH = 16;
     public static final float HEIGHT = 16;
+    private static SpriteSheet sheet;
+    protected float angle;
+    protected float speed;
+    protected Bar stickingTarget;
+    protected float relative;
+    private Image sprite;
 
 
-    public Ball(float initial_x, float initial_y, float initial_dx, float initial_dy, int sprite_number) {
+    public Ball(float initial_x, float initial_y, float angle, float speed, int sprite_number) {
         x = initial_x;
         y = initial_y;
-        dx = initial_dx;
-        dy = initial_dy;
+        this.angle = angle;
+        this.speed = speed;
         width = WIDTH;
         height = HEIGHT;
-        try {
 
+        try {
             Image img = new Image("resources/ball.png");
-            sheet = new SpriteSheet(img, (int)WIDTH, (int)HEIGHT);
+            sheet = new SpriteSheet(img, (int) WIDTH, (int) HEIGHT);
             sprite = sheet.getSprite(sprite_number, 0);
         } catch (SlickException e) {
             e.printStackTrace();
@@ -40,24 +40,26 @@ public class Ball extends RectangleGameObject {
         g.drawImage(sprite, x, y);
 
     }
+
     //barにballをくっつける
-    public void stick(Bar bar){
-        relative = x -(bar.getX());
+    public void stick(Bar bar) {
+        relative = x - (bar.getX());
         stickingTarget = bar;
 
     }
 
     public void update(GameContainer container, int delta) {
 //Todo 跳ね返った時の処理をちゃんとする！！！！
-        x += dx * delta;
-        y += dy * delta;
+
+        x += (float) Math.sin(angle + Math.PI * 1 / 2) * speed * delta;
+        y += (float) Math.cos(angle + Math.PI * 1 / 2) * speed * delta;
         List<IGameObject> targets = Main.instance.hitTest(this);
         targets = targets.stream().filter(gameObject -> !(gameObject instanceof Ball)).collect(Collectors.toList());
 
-            if (stickingTarget !=null) {
-                x = stickingTarget.getX() + relative;
-                y = stickingTarget.getY() - height;
-                Input input = container.getInput();
+        if (stickingTarget != null) {
+            x = stickingTarget.getX() + relative;
+            y = stickingTarget.getY() - height;
+            Input input = container.getInput();
             if (input.isKeyDown(Input.KEY_SPACE)) {
                 stickingTarget = null;
                 return;
@@ -68,40 +70,47 @@ public class Ball extends RectangleGameObject {
             boolean leftBottom = targets.stream().anyMatch(gameObject -> gameObject.isHitPosition(x, y + height));
             boolean rightUpper = targets.stream().anyMatch(gameObject -> gameObject.isHitPosition(x + width, y));
             boolean rightBottom = targets.stream().anyMatch(gameObject -> gameObject.isHitPosition(x + width, y + height));
-            x -= dx * delta;
-            y -= dy * delta;
+            x -= (float) Math.cos(angle) * speed * delta;
+            y -= (float) Math.sin(angle) * speed * delta;
             //上に跳ね返るとき
-            if ((leftBottom && !(leftUpper))&&dy>0) {
-                dy *= -1;
-            } else if ((rightBottom && !(rightUpper))&&dy>0) {
-                dy *= -1;
+            if ((leftBottom && !(leftUpper)) && (angle <= Math.PI / 2 || angle > Math.PI * 3 / 2)) {
+                angle = (float) Math.PI - angle;
+
+            } else if ((rightBottom && !(rightUpper)) && (angle <= Math.PI / 2 || angle > Math.PI * 3 / 2)) {
+                angle = (float) Math.PI - angle;
+
             }
             //下に跳ね返るとき
-            if ((leftUpper && !(leftBottom))&&dy<0) {
-                dy *= -1;
-            } else if ((rightUpper && !(rightBottom))&&dy<0) {
-                dy *= -1;
+            if ((leftUpper && !(leftBottom)) && (angle > Math.PI / 2 && angle <= Math.PI * 3 / 2)) {
+                angle = (float) Math.PI - angle;
+
+            } else if ((rightUpper && !(rightBottom)) && (angle > Math.PI / 2 && angle <= Math.PI * 3 / 2)) {
+                angle = (float) Math.PI - angle;
+
             }
             //右に跳ね返るとき
-            if ((leftUpper && !(rightUpper))&&dx<0) {
-                dx *= -1;
-            } else if ((leftBottom && !(rightBottom))&&dx<0) {
-                dx *= -1;
+            if ((leftUpper && !(rightUpper)) && (angle > Math.PI && angle <= Math.PI * 2)) {
+                angle = (float) Math.PI * 2 - angle;
+            } else if ((leftBottom && !(rightBottom)) && (angle > Math.PI && angle <= Math.PI * 2)) {
+                angle = (float) Math.PI * 2 - angle;
             }
             //左に跳ね返るとき
-            if ((rightUpper && !(leftUpper))&&dx>0) {
-                dx *= -1;
-            } else if ((rightBottom && !(leftBottom))&&dx>0) {
-                dx *= -1;
+            if ((rightUpper && !(leftUpper)) && (angle <= Math.PI && angle > 0)) {
+                angle = (float) Math.PI * 2 - angle;
+            } else if ((rightBottom && !(leftBottom)) && (angle <= Math.PI && angle > 0)) {
+                angle = (float) Math.PI * 2 - angle;
             }
-            x +=dx*delta;
-            y+=dy*delta;
+            if (angle < 0) {
+                angle += 2 * Math.PI;
+            }
+
+            x += (float) Math.cos(angle) * speed * delta;
+            y += (float) Math.sin(angle) * speed * delta;
             for (IGameObject target : targets) {
                 if (target instanceof IBallHitListener) {
                     ((IBallHitListener) target).onBallHit();
                 }
             }
-
         }
 
 
