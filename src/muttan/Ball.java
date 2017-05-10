1068bf662550f357301af1a33a8ad7272320280c
muttan <muttanmaiden@gmail.com>
2017-05-10 22:21:05 +0900

    リファクタリング

--- src/muttan/Ball.java
+++ src/muttan/Ball.java
@@ -12,7 +12,9 @@ public class Ball extends RectangleGameObject {
     public static final float WIDTH = 16;
     public static final float HEIGHT = 16;
     private static SpriteSheet sheet;
-    /** 右基準で左回り*/
+    /**
+     * 右基準で左回り
+     */
     protected float angle;
     protected float speed;
     //自分がくっついている先
@@ -51,73 +53,95 @@ public class Ball extends RectangleGameObject {
 
     }
 
-    public void update(GameContainer container, int delta) {
-//Todo 跳ね返るメソッドを作る！！！！
-
+    //移動処理
+    private void move(int delta) {
         x += (float) Math.cos(angle) * speed * delta;
         y += (float) -Math.sin(angle) * speed * delta;
+    }
+
+    //barについていくやつ
+    private void moveWithBar(GameContainer container) {
+        x = stickingTarget.getX() + relative;
+        y = stickingTarget.getY() - height;
+        Input input = container.getInput();
+        if (input.isKeyDown(Input.KEY_SPACE)) {
+            stickingTarget = null;
+            return;
+        }
+    }
+
+    //当たり判定
+    private List hitTest() {
         List<IGameObject> targets = Main.instance.hitTest(this);
         targets = targets.stream().filter(gameObject -> !(gameObject instanceof Ball)).collect(Collectors.toList());
+        return targets;
+    }
 
-        if (stickingTarget != null) {
-            x = stickingTarget.getX() + relative;
-            y = stickingTarget.getY() - height;
-            Input input = container.getInput();
-            if (input.isKeyDown(Input.KEY_SPACE)) {
-                stickingTarget = null;
-                return;
-            }
-        }
-        //何かにあたっていた時
-        if (!targets.isEmpty()) {
-            boolean leftUpper = targets.stream().anyMatch(gameObject -> gameObject.isHitPosition(x, y));
-            boolean leftBottom = targets.stream().anyMatch(gameObject -> gameObject.isHitPosition(x, y + height));
-            boolean rightUpper = targets.stream().anyMatch(gameObject -> gameObject.isHitPosition(x + width, y));
-            boolean rightBottom = targets.stream().anyMatch(gameObject -> gameObject.isHitPosition(x + width, y + height));
-            //当たる前の状態に戻す
-            x -= (float) Math.cos(angle) * speed * delta;
-            y -= (float) -Math.sin(angle) * speed * delta;
-            //上に跳ね返るとき
-            if ((leftBottom && !(leftUpper)) && (angle >= Math.PI  && angle < Math.PI * 2)) {
-                angle = (float) Math.PI *2- angle;
-
-            } else if ((rightBottom && !(rightUpper)) && (angle >= Math.PI  && angle < Math.PI * 2)) {
-                angle = (float) Math.PI*2 - angle;
-
-            }
-            //下に跳ね返るとき
-            if ((leftUpper && !(leftBottom)) && !(angle >= Math.PI  && angle < Math.PI * 2)) {
-                angle = (float) Math.PI*2 - angle;
-            } else if ((rightUpper && !(rightBottom)) && !(angle >= Math.PI  && angle < Math.PI * 2)) {
-                angle = (float) Math.PI*2 - angle;
-
-            }
-            //右に跳ね返るとき
-            if ((leftUpper && !(rightUpper)) && (angle >= Math.PI/2 && angle < Math.PI * 3/2)) {
-                angle = (float) Math.PI  - angle;
-            } else if ((leftBottom && !(rightBottom)) && (angle >= Math.PI/2 && angle < Math.PI * 3/2)) {
-                angle = (float) Math.PI  - angle;
-            }
-            //左に跳ね返るとき
-            if ((rightUpper && !(leftUpper)) && !(angle >= Math.PI/2 && angle < Math.PI * 3/2)) {
-                angle = (float) Math.PI  - angle;
-            } else if ((rightBottom && !(leftBottom)) && !(angle >= Math.PI/2 && angle < Math.PI * 3/2)) {
-                angle = (float) Math.PI  - angle;
-            }
-            if (angle < 0) {
-                angle += 2 * Math.PI;
-            }
-
-            x += (float) Math.cos(angle) * speed * delta;
-            y += (float) -Math.sin(angle) * speed * delta;
-            for (IGameObject target : targets) {
-                if (target instanceof Block) {
-                    ((IBallHitListener) target).onBallHit();
-                }
-
-            }
+    //上下にぶつかってたら跳ね返る処理
+    private void reboundVertical(HitTestResult hitTestResult) {
+        boolean c1 = (angle >= Math.PI && angle < Math.PI * 2);
+        //上に跳ね返るとき
+        if (c1 && hitTestResult.bottom()) {
+            angle = (float) Math.PI * 2 - angle;
         }
 
+        //下に跳ね返るとき
+        if (!c1 && hitTestResult.upper()) {
+            angle = (float) Math.PI * 2 - angle;
+        }
+    }
 
+    //左右にぶつかってたら跳ね返る処理
+    private void reboundHorizontal(HitTestResult hitTestResult) {
+        boolean c2 = (angle >= Math.PI / 2 && angle < Math.PI * 3 / 2);
+        //右に跳ね返るとき
+        if (c2 && hitTestResult.left()) {
+            angle = (float) Math.PI - angle;
+        }
+        //左に跳ね返るとき
+        if (!c2 && hitTestResult.right()) {
+            angle = (float) Math.PI - angle;
+        }
+    }
+
+    //跳ね返り処理
+    private void rebound(List<IGameObject> targets) {
+        boolean leftUpper = targets.stream().anyMatch(gameObject -> gameObject.isHitPosition(x, y));
+        boolean leftBottom = targets.stream().anyMatch(gameObject -> gameObject.isHitPosition(x, y + height));
+        boolean rightUpper = targets.stream().anyMatch(gameObject -> gameObject.isHitPosition(x + width, y));
+        boolean rightBottom = targets.stream().anyMatch(gameObject -> gameObject.isHitPosition(x + width, y + height));
+
+        HitTestResult hitTestResult = new HitTestResult(leftUpper, leftBottom, rightUpper, rightBottom);
+
+        reboundVertical(hitTestResult);
+        reboundHorizontal(hitTestResult);
+
+        if (angle < 0) {
+            angle += 2 * Math.PI;
+        }
+
+        for (IGameObject target : targets) {
+            if (target instanceof Block) {
+                ((IBallHitListener) target).onBallHit();
+            }
+        }
+    }
+
+    public void update(GameContainer container, int delta) {
+        if (stickingTarget == null) {
+            move(delta);
+
+            List<IGameObject> targets = hitTest();
+
+            //何かにあたっていた時
+            if (!targets.isEmpty()) {
+                //当たる前の状態に戻す
+                move(-delta);
+                rebound(targets);
+                move(delta);
+            }
+        } else {
+            moveWithBar(container);
+        }
     }
 }
